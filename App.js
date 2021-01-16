a579fc8e1ad1b0d491b69f62f7cb5fc1ef837879
Tommy <t3suen@uwaterloo.ca>
2021-01-16 07:17:57 -0500

    Trial with just getting an image to appear

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Watcha doin.</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// import {DisplayAnImage} from './Image.js'
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Watcha doin.</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    paddingTop: 50,
+    width: null,
+    height: null,
+    resizeMode: 'cover'
   },
 });
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.logo}>
+        <Image
+          style={styles.tinyLogo}
+          source={require('./Images/Test.jpg')}
+        />
+      </View>
+    );
+  }
+}
+

52a9e37f2dc0c265b0ad4ae2915a47b0c453ca9d
Aarifa Gowani <aarifagowani@hotmail.com>
2021-01-16 14:33:58 -0600

    add login and password

--- App.js
+++ App.js
@@ -22,62 +22,111 @@
 // });
 
 import React from 'react';
-import { View, Image, StyleSheet, TextInput } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 50, // we don't need this, as our logo will be in the center?
-    flex: 1,
-    backgroundColor: '#A4508B',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
-  logo: {
-    paddingTop: 50,
-    width: null,
-    height: null,
-    resizeMode: 'cover'
-  },
-  inputText: {
-
-
-
-  }
-});
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
-  state= {
+  state={
     username: "",
     password: ""
   }
   render() {
     return (
-      // this is where the logo picture will go?
-      <View style={styles.logo}> 
-        <Image
-          style={styles.tinyLogo}
-          source={require('./Images/Test.jpg')}
-        />
+      <View style={styles.container}>
+        <Text style={styles.logo}>SIGN</Text>
+        <View style={styles.inputView} >
+          <TextInput
+            style={styles.inputText}
+            placeholder="Username..."
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
         <View style={styles.inputView}>
           <TextInput
-            style= {styles.inputText}
-            placeholder= "Username..."
-            placeholderTextColor= "#590799"
-            onChangeText={text => this.setState({username:text})}/>
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Password..."
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({password:text})}/>
         </View>
-        <View style={styles.inoutView}>
-        <TextInput
-          secureTextEntry
-          style={styles.inoutText}
-          placeholder="Password..."
-          placeholderTextColor= "#590799"
-          onChangeText={text => this.setState({password:text})}/>
-        </View>
+        <TouchableOpacity>
+          <Text style={styles.forgot}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.loginText}>Sign up</Text>
+        </TouchableOpacity>
       </View>
+
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1e90ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize:50,
+    color:"#fb5b5a",
+    marginBottom: 40
+  },
+  inputView: {
+    width:"80%",
+    backgroundColor: "#465881",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20
+  },
+  inputText: {
+    height:50,
+    color: "white"
+  },
+  forgot: {
+    color: "white",
+    fontSize: 11
+  },
+  loginBtn: {
+    width:"80%",
+    backgroundColor: "#fb5b5a",
+    borderRadius: 25,
+    heigth: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color: "white"
+  }
+});
+
+
+
+//  inputView: {
+//    width: "80%",
+//    backgroundColor:
+//    borderRadius:25;
+//    height: 50,
+//    marginBottom: 20,
+//    justifyContent: "center",
+//    padding: 20
+//   }
+
+// tinyLogo: {
+  //   width: 50,
+  //   height: 50,
+  // },
+
+  // logo: {
+  //   paddingTop: 50,
+  //   width: null,
+  //   height: null,
+  //   resizeMode: 'cover'
